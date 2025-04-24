faa71103702d70c7a3e224afa84cbb40d145ff6f
TKIN10 <ace.1031.3d2y@icloud.com>
2025-04-24 18:55:12 +0900

    No.10 Java 基本構文編 クラス

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 29;
+		double height = 170;
+		GeekIntroduction.greeting(age, height);
+		
+		int num1 = 2;
+		int num2 = 50;
+		GeekIntroduction.specialSkill(num1, num2);
+	}
+}
